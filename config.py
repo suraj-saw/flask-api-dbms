851468c20bf05d8dba90d2ec838ebdbbec6b3e4e
Suraj Kumar Saw <suraj03saw@gmail.com>
2024-11-10 10:47:11 +0530

    config.py

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
-import os
-
 class Config:
-    MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost.render.com')
-    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'Suraj03saw@')
-    MYSQL_DB = os.getenv('MYSQL_DB', 'project')
-    MYSQL_PORT = int(os.getenv('MYSQL_PORT', 3306))  # Default to 3306 if not set
+    MYSQL_HOST = "localhost.render.com"  # The hostname for the MySQL database
+    MYSQL_USER = "root"
+    MYSQL_PASSWORD = "Suraj03saw@"
+    MYSQL_DB = "project"
+    MYSQL_PORT = 3306  # Default MySQL port
