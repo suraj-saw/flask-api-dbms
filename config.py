473c078d684c5b578d5ebd87b3965ca2e5513e08
Suraj Kumar Saw <suraj03saw@gmail.com>
2024-11-15 10:27:16 +0530

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 class Config:
-    MYSQL_HOST = "2409:4050:2e3a:a2fc:f5f6:8468:9970:4dd2" 
-    MYSQL_USER = "suraj"
-    MYSQL_PASSWORD = "Suraj03saw@"
-    MYSQL_DB = "project"
-    MYSQL_PORT = 3306  # Default MySQL port
+    MYSQL_HOST = '192.168.78.42'
+    MYSQL_USER = 'suraj'
+    MYSQL_PASSWORD = 'Suraj03saw@'
+    MYSQL_DB = 'project'
+    MYSQL_PORT = 3306
+    MYSQL_SSL_DISABLED = True  # Disable SSL
+    MYSQL_SSL_CA = ''  # Empty CA to ensure no SSL is used
